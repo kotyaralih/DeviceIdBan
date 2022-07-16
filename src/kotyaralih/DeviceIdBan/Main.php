97b0fabd816e392bbee3933d44d2c328a7d891e8
Ace Sobremont <100104970+xqwtxon@users.noreply.github.com>
2022-07-16 13:56:59 +0800

    @return void

--- src/kotyaralih/DeviceIdBan/Main.php
+++ src/kotyaralih/DeviceIdBan/Main.php
@@ -30,7 +30,7 @@ class Main extends PluginBase implements Listener{
 		}
 	}
 	
-	public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) :bool {
+	public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) :void { // should be non-returnable
 		if(strtolower($cmd->getName()) == "deviceban"){
 			if(count($args) < 2){
 				if(!$sender instanceof Player){
